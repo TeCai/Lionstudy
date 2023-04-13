05f5ad27f46aa7670fe34dfb1c9ec866d0cfff3e
TeCai <xiang_zhengrui@outlook.com>
2023-04-13 04:55:32 +0100

    mylion

---
+++ mylion_pytorch/mylion_pytorch.py
@@ -0,0 +1,98 @@
+from typing import Tuple, Optional, Callable
+
+import torch
+from torch.optim.optimizer import Optimizer
+
+# functions
+
+def exists(val):
+    return val is not None
+
+# update functions
+
+def update_fn(p, grad, exp_avg, lr, wd, beta1, beta2):
+    # stepweight decay
+
+    p.data.mul_(1 - lr * wd)
+
+    # compute the norm of the gradient before the sign operation
+    # grad_norm = torch.norm(grad)
+    # print(grad_norm)
+
+    # weight update
+    # signed_grad = grad.sign()
+    
+    # adjust the norm of the signed gradient
+    # adjusted_signed_grad = signed_grad * 1./((grad_norm+1).log()+0.99)
+
+    update_m = exp_avg.clone().mul_(beta1).add(grad, alpha = 1 - beta1).sign_()
+    update_norm = torch.norm(update_m).sqrt()
+    update = update_m * 1./((update_norm+1).log()+0.99)
+
+    p.add_(update, alpha = -lr)
+
+    # decay the momentum running average coefficient
+    exp_avg.mul_(beta2).add_(grad, alpha = 1 - beta2)
+
+# class
+
+class MyLion(Optimizer):
+    def __init__(
+        self,
+        params,
+        lr: float = 1e-4,
+        betas: Tuple[float, float] = (0.9, 0.99),
+        weight_decay: float = 0.0,
+        use_triton: bool = False
+    ):
+        assert lr > 0.
+        assert all([0. <= beta <= 1. for beta in betas])
+
+        defaults = dict(
+            lr = lr,
+            betas = betas,
+            weight_decay = weight_decay
+        )
+
+        super().__init__(params, defaults)
+
+        self.update_fn = update_fn
+
+        if use_triton:
+            from lion_pytorch.triton import update_fn as triton_update_fn
+            self.update_fn = triton_update_fn
+
+    @torch.no_grad()
+    def step(
+        self,
+        closure: Optional[Callable] = None
+    ):
+
+        loss = None
+        if exists(closure):
+            with torch.enable_grad():
+                loss = closure()
+
+        for group in self.param_groups:
+            for p in filter(lambda p: exists(p.grad), group['params']):
+
+                grad, lr, wd, beta1, beta2, state = p.grad, group['lr'], group['weight_decay'], *group['betas'], self.state[p]
+
+                # init state - exponential moving average of gradient values
+
+                if len(state) == 0:
+                    state['exp_avg'] = torch.zeros_like(p)
+
+                exp_avg = state['exp_avg']
+
+                self.update_fn(
+                    p,
+                    grad,
+                    exp_avg,
+                    lr,
+                    wd,
+                    beta1,
+                    beta2
+                )
+
+        return loss
